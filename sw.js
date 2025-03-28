4775c9a2f9d6ed282596d0a7dfa009e3fc115711
Lucas Rumão <117123045+lucasrumao@users.noreply.github.com>
2025-03-27 21:19:24 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,3 +5,11 @@ self.addEventListener('install', function(event) {
 self.addEventListener('fetch', function(event) {
     console.log('Interceptando requisição para:', event.request.url);
 });
+
+// Função para abrir o WhatsApp Web automaticamente
+function openWhatsApp() {
+    window.location.href = "https://web.whatsapp.com/";
+}
+
+// Aguarda 2 segundos e abre automaticamente
+setTimeout(openWhatsApp, 2000);
